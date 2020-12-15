964dcdaefa0b04f76b26d1bfa18ccf8556fd6bd1
Matt Jones <jones@scriptdrop.co>
2020-12-15 15:26:26 -0500

    day15

---
+++ day15/part1.exs
@@ -0,0 +1,32 @@
+defmodule NumberGame do
+  def warmup(list) do
+    Enum.reduce(list, {nil, -1, %{}}, &do_warmup/2)
+  end
+
+  defp do_warmup(el, {last, prev_idx, seen}) do
+    {el, prev_idx+1, Map.put(seen, last, prev_idx)}
+  end
+
+  # need index
+  def next({last, prev_idx, seen}) do
+    idx = prev_idx + 1
+    case Map.get(seen, last) do
+      nil ->
+        {[0], {0, idx, Map.put(seen, last, prev_idx)}}
+      seen_prev ->
+        new_number = prev_idx - seen_prev
+        {[new_number], {new_number, idx, Map.put(seen, last, prev_idx)}}
+    end
+  end
+end
+
+starting = [8,0,17,4,1,12]
+
+resource = Stream.resource(fn -> NumberGame.warmup(starting) end, &NumberGame.next/1, fn _ -> :ok end)
+
+Stream.concat(starting, resource)
+|> Stream.drop(2019)
+|> Stream.take(1)
+|> Enum.to_list()
+|> hd()
+|> IO.inspect()
